6729aaadc2623fa0d89158a5e229ef9e5605f22f
Donald Pakkies <donaldpakkies@gmail.com>
2018-11-17 07:53:24 +0200

    Feat: map the caller to the exception
    docblock

--- Exceptions/HibernateBuilderException.php
+++ Exceptions/HibernateBuilderException.php
@@ -6,8 +6,24 @@ use Exception;
 
 class HibernateBuilderException extends Exception
 {
+  /**
+   * $message
+   *
+   * @var string
+   */
+  protected $message = 'Returned value is not a real Eloquent query.';
+
+  /**
+   * __construct
+   *
+   * @return void
+   */
   public function __construct()
   {
-    $this->message = 'Returned value is not a real Eloquent query.';
+    $args = debug_backtrace()[1];
+
+    foreach ($args as $key => $value) {
+      $this->{$key} = $value;
+    }
   }
 }
