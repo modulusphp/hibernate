88d6fdfb4af961da4cc9367d0e9fa1715fcce418
Donald Pakkies <donald@sov.tech>
2019-11-08 00:35:50 +0200

    feat: Get log level number

--- src/Logging/Mocks/HasLogLevel.php
+++ src/Logging/Mocks/HasLogLevel.php
@@ -2,6 +2,7 @@
 
 namespace Modulus\Hibernate\Logging\Mocks;
 
+use Monolog\Logger;
 use Modulus\Support\Config;
 
 trait HasLogLevel
@@ -14,5 +15,51 @@ trait HasLogLevel
   public function getName() : string
   {
     return $this->default ?? Config::get('logging.default');
+  }
+
+  /**
+   * Get log level number
+   *
+   * @return int
+   */
+  private function getLevelNumber($value) : int
+  {
+    switch ($value) {
+      case 'debug':
+        return Logger::DEBUG;
+        break;
+
+      case 'info':
+        return Logger::INFO;
+        break;
+
+      case 'notice':
+        return Logger::NOTICE;
+        break;
+
+      case 'warning':
+        return Logger::WARNING;
+        break;
+
+      case 'error':
+        return Logger::ERROR;
+        break;
+      
+      case 'critical':
+        return Logger::CRITICAL;
+        break;
+
+      case 'alert':
+        return Logger::ALERT;
+        break;
+
+      case 'emergency':
+        return Logger::EMERGENCY;
+        break;
+
+      default:
+        return Logger::DEBUG;
+        break;
+    }
   }
 }
