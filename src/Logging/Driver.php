4ea724591e9ed46b7b6e5bf418ac00c334d7ca0f
Donald Pakkies <donald@sov.tech>
2019-11-08 00:39:37 +0200

    feat: Make logger
    instantiate a new monolog class and add handlers

--- src/Logging/Driver.php
+++ src/Logging/Driver.php
@@ -2,11 +2,38 @@
 
 namespace Modulus\Hibernate\Logging;
 
+use Monolog\Logger;
 use Modulus\Support\Config;
 use Monolog\Handler\HandlerInterface;
+use Modulus\Hibernate\Logging\Mocks\{
+  HasLogLevel,
+  HasFormatting,
+  HasManyHandlers,
+};
 
 class Driver
 {
+  use HasLogLevel;
+  use HasFormatting;
+  use HasManyHandlers;
+
+  /**
+   * Make logger
+   *
+   * @return Logger
+   */
+  public function get() : Logger
+  {
+    $log = new Logger($this->getEnvironment());
+
+    $handler = $this->handler();
+    $handler->setFormatter($this->formatter());
+
+    $log->pushHandler($handler);
+
+    return $this->default ? $log : $this->withHandlers($log);
+  }
+
   /**
    * Register handler
    *
