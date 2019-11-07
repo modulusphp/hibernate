5d17b59d23b499c9c256f1b7fb02654026a98ddf
Donald Pakkies <donald@sov.tech>
2019-11-08 00:47:09 +0200

    feat: Create log file if it doesn't already exists

--- src/Logging/Drivers/Daily.php
+++ src/Logging/Drivers/Daily.php
@@ -8,6 +8,16 @@ use Modulus\Hibernate\Logging\Driver;
 class Daily extends Driver
 {
   /**
+   * Create log file if it doesn't already exists
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    if (!file_exists($this->getLogFile())) touch($this->getLogFile());
+  }
+
+  /**
    * Get log file
    *
    * @return string
