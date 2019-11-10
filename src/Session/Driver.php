feb2e00be71ff4e6c94dfba9fada1fa45f0585db
Donald Pakkies <donald@sov.tech>
2019-11-10 09:57:07 +0200

    feat: Register driver

---
+++ src/Session/Driver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modulus\Hibernate\Session;
+
+use Sesshin\Store\StoreInterface;
+
+class Driver
+{
+  /**
+   * Register driver
+   *
+   * @return StoreInterface
+   */
+  public function handler() : StoreInterface
+  {
+    //
+  }
+}
