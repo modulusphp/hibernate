2b7dc09eceb597828c06bbd9ba2d38788918fffb
Donald Pakkies <donald@sov.tech>
2019-11-10 10:10:44 +0200

    feat: Get cache driver

--- src/Session/SessionBase.php
+++ src/Session/SessionBase.php
@@ -27,4 +27,15 @@ class SessionBase
     'file' => \Modulus\Hibernate\Session\Drivers\File::class,
     'redis' => \Modulus\Hibernate\Session\Drivers\Redis::class,
   ];
+
+  /**
+   * Get cache driver
+   *
+   * @param string $driver
+   * @return mixed
+   */
+  private function getDriver(string $driver)
+  {
+    return isset(self::$supported[$driver]) ? $driver : null;
+  }
 }
