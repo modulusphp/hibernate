81a8600697fee16dfa5a37d965136ad34bac86fd
Donald Pakkies <donald@sov.tech>
2019-11-10 09:58:22 +0200

    feat: Make session

--- src/Session/Driver.php
+++ src/Session/Driver.php
@@ -2,11 +2,28 @@
 
 namespace Modulus\Hibernate\Session;
 
+use Sesshin\User\Session;
 use Modulus\Support\Config;
 use Sesshin\Store\StoreInterface;
 
 class Driver
 {
+  /**
+   * Make session
+   *
+   * @return Session
+   */
+  public function get() : Session
+  {
+    $session = new Session($this->handler());
+
+    $session->setIdHandler(new Handler);
+
+    $session->open(true);
+
+    return $session;
+  }
+
   /**
    * Register driver
    *
