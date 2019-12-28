327f9580f49b0a1a45834921ecc9de1da590fccc
Donald Pakkies <donald@sov.tech>
2019-12-28 09:21:46 +0200

    feat: Writes a error message to the output and adds a newline at the end

--- src/Console/Write.php
+++ src/Console/Write.php
@@ -58,4 +58,16 @@ class Write
   {
     return $this->output->writeln("<info>{$message}</info>", $options);
   }
+
+  /**
+   * Writes a error message to the output and adds a newline at the end
+   *
+   * @param string|iterable $messages The message as an iterable of strings or a single string
+   * @param bool $newline Whether to add a newline
+   * @return mixed
+   */
+  public function error(string $message, $options = 0)
+  {
+    return $this->output->writeln("<error>{$message}</error>", $options);
+  }
 }
