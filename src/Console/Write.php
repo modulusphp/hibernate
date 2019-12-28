a89cae9eae34d325dfed57b88e0104a2557ca82a
Donald Pakkies <donald@sov.tech>
2019-12-28 09:20:40 +0200

    feat: Writes a message to the output

--- src/Console/Write.php
+++ src/Console/Write.php
@@ -21,4 +21,17 @@ class Write
   {
     $this->output = $output;
   }
+
+  /**
+   * Writes a message to the output
+   *
+   * @param string|iterable $messages The message as an iterable of strings or a single string
+   * @param bool $newline Whether to add a newline
+   * @param int $options A bitmask of options (one of the OUTPUT or VERBOSITY constants), 0 is considered the same as self::OUTPUT_NORMAL | self::VERBOSITY_NORMAL
+   * @return mixed
+   */
+  public function write($messages, bool $newline = false, int $options = 0)
+  {
+    return $this->output->write($messages, $newline, $options);
+  }
 }
